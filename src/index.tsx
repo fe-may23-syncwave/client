4a3aeddade2cca608a87d5559817f41000ae615f
Olena Boldysheva <adv55577@gmail.com>
2023-08-25 14:43:45 +0300

    initial

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './styles/main.scss';
+import App from './App';
+
+createRoot(document.getElementById('root') as HTMLElement).render(<App />);
