740b1d0274def7d5b459daa7d657ef4f8559fb33
Olena Boldysheva <adv55577@gmail.com>
2023-08-29 14:04:22 +0300

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,16 @@
-import { Container } from 'components/common/Container';
 import React from 'react';
+import { Container } from 'components/common/Container';
+import classNames from 'classnames';
+import { Logo } from 'components/common/Logo';
+import { ReactComponent as ArrowUpIcon } from 'assets/icons/arrow_up.svg';
+
+import styles from './Footer.module.scss';
+
+export enum FooterLink {
+  Github = 'https://github.com/fe-may23-syncwave/client',
+  Contacts = 'contacts',
+  Rights = '#',
+}
 
 type Props = {
   className?: string;
@@ -7,8 +18,25 @@ type Props = {
 
 export const Footer: React.FC<Props> = ({ className }) => {
   return (
-    <Container className={className}>
-      <div>Footer</div>
-    </Container>
+    <div className={classNames(className, styles.Footer)}>
+      <Container>
+        <div className={styles.Footer__content}>
+          <Logo type="medium" />
+          <div className={styles.Footer__list}>
+            {Object.entries(FooterLink).map(([title, link]) => (
+              <a key={title} href={link} target="blank" rel="noreferrer">
+                {title.toUpperCase()}
+              </a>
+            ))}
+          </div>
+          <button type="button" className={styles.Footer__button}>
+            Back to top
+            <span className={styles.Footer__button__icon}>
+              <ArrowUpIcon />
+            </span>
+          </button>
+        </div>
+      </Container>
+    </div>
   );
 };
