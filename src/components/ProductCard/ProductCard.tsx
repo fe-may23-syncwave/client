8fe3f7784d4f470a43817522c20cfbd467bdc015
dianadiv <diana.gs202@gmail.com>
2023-08-29 16:41:30 +0300

    add CatalogPage with List

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -2,41 +2,50 @@ import './ProductCard.scss';
 import { Link } from 'react-router-dom';
 import { FavouritesButton } from '../FavouritesButton';
 import { AddToCartButton } from '../AddToCartButton';
+import { Phone } from '../../types/PhoneType';
 
-export const ProductCard = () => {
+interface Props{
+  product: Phone,
+}
+
+export const ProductCard: React.FC<Props> = ({ product }) => {
   return (
     <li className="product">
       <Link
-        to="`/phones/apple-iphone-11-128gb-purple"
+        to={product.phoneId}
         className="product__link"
       >
         <img
-          src={`${process.env.PUBLIC_URL}/img/phones/apple-iphone-11/purple/00.jpg`}
-          alt="Apple iPhone 11 128GB Purple (iMT9G2FS/A)"
+          src={`${process.env.PUBLIC_URL}/${product.image}`}
+          alt={product.name}
           className="product__image"
         />
         <h2 className="product__title">
-          Apple iPhone 11 128GB Purple (iMT9G2FS/A)
+          {`${product.name} (XXXXXXXX)`}
         </h2>
       </Link>
 
       <div className="product__prices">
-        <p className="product__price">$799</p>
-        <p className="product__price-full">$899</p>
+        <p className="product__price">
+          {`$${product.price}`}
+        </p>
+        <p className="product__price-full">
+          {`$${product.fullPrice}`}
+        </p>
       </div>
 
       <div className="product__info">
         <div className="product__info-block">
           <p className="product__info-title">Screen</p>
-          <p className="product__info-data">6.2&quot; IPS</p>
+          <p className="product__info-data">{product.screen}</p>
         </div>
         <div className="product__info-block">
           <p className="product__info-title">Capacity</p>
-          <p className="product__info-data">128 GB</p>
+          <p className="product__info-data">{product.capacity}</p>
         </div>
         <div className="product__info-block">
           <p className="product__info-title">RAM</p>
-          <p className="product__info-data">4 RAM</p>
+          <p className="product__info-data">{product.ram}</p>
         </div>
       </div>
 
