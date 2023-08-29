181e6317fec6ebd53dc367823056425ed53028a8
Yana <yanaklymenko.od@gmail.com>
2023-08-29 12:17:54 +0300

    commit merge main

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -2,7 +2,5 @@ import React from 'react';
 import { ProductCard } from '../../components/ProductCard';
 
 export const CatalogPage: React.FC = () => {
-  return (
-    <ProductCard />
-  );
+  return <ProductCard />;
 };
