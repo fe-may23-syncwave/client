02e99247916c1ae2792e5beaed13dee13baded08
Olena Boldysheva <adv55577@gmail.com>
2023-08-29 22:21:20 +0300

    add basic menu modal

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+/* eslint-disable react/jsx-wrap-multilines */
+import React, { useState } from 'react';
 import classNames from 'classnames';
-import { Navigation } from 'components/Navigation';
+// import { Navigation } from 'components/Navigation';
+import { Modal } from 'react-responsive-modal';
+import { ReactComponent as CloseIcon } from 'assets/icons/close.svg';
 import { Logo } from '../common/Logo';
+import 'react-responsive-modal/styles.css';
 import styles from './Header.module.scss';
 
 type Props = {
@@ -9,11 +13,49 @@ type Props = {
 };
 
 export const Header: React.FC<Props> = ({ className }) => {
+  const [open, setOpen] = useState(true);
+
+  // const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
   return (
     <div className={classNames(styles.Header, className)}>
       <Logo className={styles.Header__logo} type="small" />
 
-      <Navigation />
+      {/* <button type="button" onClick={onOpenModal}>
+        Open modal
+      </button> */}
+      <Modal
+        open={open}
+        onClose={onCloseModal}
+        center
+        closeIcon={<CloseIcon />}
+        classNames={{
+          overlay: styles.Overlay,
+          modal: styles.Modal,
+          modalAnimationIn: styles.customEnterOverlayAnimation,
+          modalAnimationOut: styles.customLeaveOverlayAnimation,
+        }}
+        animationDuration={800}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            height: '100%',
+          }}
+        >
+          <div className={classNames(styles.Header, className)}>
+            <Logo className={styles.Header__logo} type="small" />
+          </div>
+          {/* <Navigation /> */}
+          <div>List</div>
+          <div>Footer</div>
+        </div>
+      </Modal>
+
+      {/* <Navigation /> */}
     </div>
   );
 };
