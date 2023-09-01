04bf648a545e46a298e3c3fab4e0fdf197e07dfb
dianadiv <diana.gs202@gmail.com>
2023-09-01 12:47:20 +0300

    fix colors in dark theme and add not found page

--- src/components/BackButton/BackButton.tsx
+++ src/components/BackButton/BackButton.tsx
@@ -1,13 +1,18 @@
 import { useNavigate } from 'react-router-dom';
 import { ReactComponent as ArrowBackIcon } from 'assets/icons/arrow_back.svg';
+import { ReactComponent as ArrowBackLight } from 'assets/icons/arrow-light.svg';
 import './BackButton.scss';
+import { MainContext } from 'context/MainContext';
+import React from 'react';
 
 export const BackButton: React.FC = () => {
   const navigate = useNavigate();
 
+  const { darkTheme } = React.useContext(MainContext);
+
   return (
     <button type="button" className="back-button" onClick={() => navigate(-1)}>
-      <ArrowBackIcon />
+      { darkTheme ? <ArrowBackLight /> : <ArrowBackIcon />}
     </button>
   );
 };
