b51cbe256b1f79306c137e589a58cdc9ceea14dc
Yana Klymenko <134420308+klmyana@users.noreply.github.com>
2023-09-04 18:54:54 +0300

    fixed

--- src/components/ProductColors/ProductColors.tsx
+++ src/components/ProductColors/ProductColors.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom';//
+import { Link, useLocation } from 'react-router-dom'; //
 import classNames from 'classnames';
 import './ProductColors.scss';
 
