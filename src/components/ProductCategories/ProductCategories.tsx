45c07de71d6aa3d20a5b03e62cf3048d7c54c0de
fawnique <stolpnik.s.i@gmail.com>
2023-09-05 14:17:28 +0300

    add basic ProductCategories

---
+++ src/components/ProductCategories/ProductCategories.tsx
@@ -0,0 +1,51 @@
+import { Link } from 'react-router-dom';
+import './ProductCategories.scss'; // Direct import
+import phonesImage from '../../assets/images/categories/category-phones.png';
+import tabletsImage from '../../assets/images/categories/category-tablets.png';
+// eslint-disable-next-line max-len
+import accessoriesImage from '../../assets/images/categories/category-accessories.png';
+
+export const ProductCategories = () => {
+  return (
+    <div className="product-categories">
+      <h2 className="product-categories__title">Shop by category</h2>
+      <div className="product-categories__list">
+        <Link to="/phones" className="product-categories__item">
+          <div className="product-categories__image-wrapper">
+            <img
+              src={phonesImage}
+              alt="phones"
+              className="product-categories__image"
+            />
+          </div>
+          <h3 className="product-categories__subtitle">Phones</h3>
+          <p className="product-categories__description">95 models</p>
+        </Link>
+
+        <Link to="/tablets" className="product-categories__item">
+          <div className="product-categories__image-wrapper">
+            <img
+              src={tabletsImage}
+              alt="tablets"
+              className="product-categories__image"
+            />
+          </div>
+          <h3 className="product-categories__subtitle">Tablets</h3>
+          <p className="product-categories__description">24 models</p>
+        </Link>
+
+        <Link to="/accessories" className="product-categories__item">
+          <div className="product-categories__image-wrapper">
+            <img
+              src={accessoriesImage}
+              alt="accessories"
+              className="product-categories__image"
+            />
+          </div>
+          <h3 className="product-categories__subtitle">Accessories</h3>
+          <p className="product-categories__description">100 models</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
--- src/pages/auth/index.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable object-curly-newline */
-import { RegistrationPage } from './RegistrationPage';
-import { AccountActivationPage } from './AccountActivationPage';
-import { LoginPage } from './LoginPage';
-import { UsersPage } from './UsersPage';
-
-export { RegistrationPage, AccountActivationPage, LoginPage, UsersPage };
