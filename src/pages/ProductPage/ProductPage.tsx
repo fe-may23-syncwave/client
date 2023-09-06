a8aa1eea6ef0f3a3080d570536e3a8c26b65e92b
dianadiv <diana.gs202@gmail.com>
2023-09-06 22:34:26 +0300

    fix bugs

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -38,6 +38,7 @@ export const ProductPage: React.FC = () => {
   const typeOfProducts = currentURL.split('/').reverse()[1];
 
   useEffect(() => {
+    setIsLoading(true);
     getProductByIdWihDetails(`${typeOfProducts}/${productId}`)
       .then((prod) => {
         setProduct(prod);
@@ -65,7 +66,7 @@ export const ProductPage: React.FC = () => {
         console.error('Error:', error);
       });
   }, []);
-  
+
   function getTechSpecs(productType: string, prod: ProductWithDetails) {
     if (productType === 'phones') {
       return Object.entries(prod).slice(-7);
@@ -95,154 +96,109 @@ export const ProductPage: React.FC = () => {
 
     return [];
   }
- 
+
   return (
     <>
       {isLoading && <Loader />}
       {!isLoading && !product && !prepared && <NotFoundPage />}
-      {product
-        && prepared
-        && !isLoading
-        && (
-          <>
-            <BreadCrumbs />
-            <div className="product-page">
-              <div className="product-page__content">
-                <BackButton />
+      {product && prepared && !isLoading && (
+        <>
+          <BreadCrumbs />
+          <div className="product-page">
+            <div className="product-page__content">
+              <BackButton />
 
-                <h1 className="product-page__title">{product.name}</h1>
+              <h1 className="product-page__title">{product.name}</h1>
 
-                <div className="product-page__top">
-                  <div className="product-page__photos photos">
-                    <div className="photos__sidebar">
-                      {product.images.map((image) => (
-                        <button
-                          type="button"
-                          className={classNames('photos__sidebar--wrapper', {
-                            'photos__sidebar--wrapper--active':
-                              image === mainPhoto,
-                          })}
-                          onClick={() => setMainPhoto(image)}
-                          key={image}
-                        >
-                          <img
-                            src={`${CLOUDINARY}/${image}`}
-                            alt="product"
-                            className="photos__sidebar--item"
-                          />
-                        </button>
-                      ))}
-                    </div>
-
-                    <div className="photos__main">
-                      <img
-                        src={`${CLOUDINARY}/${mainPhoto}`}
-                        alt={product.name}
-                        className="photos__main-photo"
-                      />
-                    </div>
-                  </div>
-
-                  <div className="product-page__rightbar">
-                    <div className="product-page__select-container">
-                      <div className="product-page__colors colors">
-                        <p className="product-page__subtitle">
-                          Available colors
-                        </p>
-                        <ProductColors
-                          colors={product.colorsAvailable}
-                          currentColor={product.color}
+              <div className="product-page__top">
+                <div className="product-page__photos photos">
+                  <div className="photos__sidebar">
+                    {product.images.map((image) => (
+                      <button
+                        type="button"
+                        className={classNames('photos__sidebar--wrapper', {
+                          'photos__sidebar--wrapper--active': image === mainPhoto,
+                        })}
+                        onClick={() => setMainPhoto(image)}
+                        key={image}
+                      >
+                        <img
+                          src={`${CLOUDINARY}/${image}`}
+                          alt="product"
+                          className="photos__sidebar--item"
                         />
-                      </div>
-                    </div>
-                    
-                    {product.capacity && product.capacityAvailable && (
-                      <div className="product-page__select-container">
-                        <div className="product-page__capacity capacity">
-                          <p className="product-page__subtitle">
-                            Select capacity
-                          </p>
-                          <ProductCapacity
-                            capacities={product.capacityAvailable}
-                            currentCapacity={product.capacity}
-                          />
-                        </div>
-                      </div>
-                    )}
-
-                    <div className="product-page__price">
-                      <h2 className="product-page__price--sale">
-                        {!product.discountPrice
-                          ? `$${product.fullPrice}`
-                          : `$${product.discountPrice}`}
-                      </h2>
-
-                      <p className="product-page__price--reg">
-                        {product.discountPrice ? `$${product.fullPrice}` : ''}
-                      </p>
-                    </div>
-
-                    <div className="product-page__buttons">
-                      <AddToCartButton
-                        product={prepared}
-                        styles={[
-                          'product-page__button',
-                          'product-page__button--active',
-                        ]}
-                      />
-
-                      <FavouritesButton
-                        product={prepared}
-                        styles={[
-                          'product__favourites',
-                          'product__favourites--active',
-                          'product-page__favourites',
-                        ]}
-                      />
-                      
-              <div className="product-page__description description">
-                {getTechSpecs(typeOfProducts, product)
-                  .slice(2)
-                  .map((data) => (
-                    <div key={data[0]} className="info__techspecs--content">
-                      <p className="info__techspecs--title">
-                        {data[0] === 'ram'
-                          ? data[0].toUpperCase()
-                          : capitalizeText(String(data[0]))}
-                      </p>
-                      <p className="info__techspecs--data">{String(data[1])}</p>
-                    </div>
+                      </button>
                     ))}
-                    </div>
                   </div>
 
-                  <div className="product-page__id">
-                    <p className="product-page__subtitle">
-                      {`ID: ${createId(product.id)}`}
-                    </p>
+                  <div className="photos__main">
+                    <img
+                      src={`${CLOUDINARY}/${mainPhoto}`}
+                      alt={product.name}
+                      className="photos__main-photo"
+                    />
                   </div>
                 </div>
 
-                <div className="product-page__info info">
-                  <div className="info__content">
-                    <div className="info__about">
-                      <h1 className="info__title">About</h1>
+                <div className="product-page__rightbar">
+                  <div className="product-page__select-container">
+                    <div className="product-page__colors colors">
+                      <p className="product-page__subtitle">Available colors</p>
 
-                      {product.description.map((desc) => (
-                        <Fragment key={desc.title}>
-                          <h3 className="info__about--heading" key={desc.title}>
-                            {desc.title}
-                          </h3>
-
-                          <p className="info__about--description">
-                            {desc.text}
-                          </p>
-                        </Fragment>
-                      ))}
+                      <ProductColors
+                        colors={product.colorsAvailable}
+                        currentColor={product.color}
+                      />
                     </div>
-                    <div className="info__techspecs">
-                      <h1 className="info__title">Tech specs</h1>
-                      {getTechSpecs(typeOfProducts, product).map((data) => (
+                  </div>
+
+                  {product.capacity && product.capacityAvailable && (
+                    <div className="product-page__select-container">
+                      <div className="product-page__capacity capacity">
+                        <p className="product-page__subtitle">Select capacity</p>
+                        <ProductCapacity
+                          capacities={product.capacityAvailable}
+                          currentCapacity={product.capacity}
+                        />
+                      </div>
+                    </div>
+                  )}
+
+                  <div className="product-page__price">
+                    <h2 className="product-page__price--sale">
+                      {!product.discountPrice
+                        ? `$${product.fullPrice}`
+                        : `$${product.discountPrice}`}
+                    </h2>
+
+                    <p className="product-page__price--reg">
+                      {product.discountPrice ? `$${product.fullPrice}` : ''}
+                    </p>
+                  </div>
+
+                  <div className="product-page__buttons">
+                    <AddToCartButton
+                      product={prepared}
+                      styles={[
+                        'product-page__button',
+                        'product-page__button--active',
+                      ]}
+                    />
+
+                    <FavouritesButton
+                      product={prepared}
+                      styles={[
+                        'product__favourites',
+                        'product__favourites--active',
+                        'product-page__favourites',
+                      ]}
+                    />
+                  </div>
+
+                  <div className="product-page__description description">
+                    {getTechSpecs(typeOfProducts, product)
+                      .slice(2)
+                      .map((data) => (
                         <div key={data[0]} className="info__techspecs--content">
                           <p className="info__techspecs--title">
                             {data[0] === 'ram'
@@ -252,19 +208,58 @@ export const ProductPage: React.FC = () => {
                           <p className="info__techspecs--data">{String(data[1])}</p>
                         </div>
                       ))}
-                    </div>
+                  </div>
+                </div>
+
+                <div className="product-page__id">
+                  <p className="product-page__subtitle">
+                    {`ID: ${createId(product.id)}`}
+                  </p>
+                </div>
+              </div>
+
+              <div className="product-page__info info">
+                <div className="info__content">
+                  <div className="info__about">
+                    <h1 className="info__title">About</h1>
+
+                    {product.description.map((desc) => (
+                      <Fragment key={desc.title}>
+                        <h3 className="info__about--heading" key={desc.title}>
+                          {desc.title}
+                        </h3>
+
+                        <p className="info__about--description">
+                          {desc.text}
+                        </p>
+                      </Fragment>
+                    ))}
+                  </div>
+                  <div className="info__techspecs">
+                    <h1 className="info__title">Tech specs</h1>
+                    {getTechSpecs(typeOfProducts, product).map((data) => (
+                      <div key={data[0]} className="info__techspecs--content">
+                        <p className="info__techspecs--title">
+                          {data[0] === 'ram'
+                            ? data[0].toUpperCase()
+                            : capitalizeText(String(data[0]))}
+                        </p>
+                        <p className="info__techspecs--data">{String(data[1])}</p>
+                      </div>
+                    ))}
                   </div>
                 </div>
               </div>
             </div>
+          </div>
 
-            <div className="product-page__slider">
-              {newestProducts.length > 0 && (
-                <ProductsSlider phones={newestProducts} title="Hot prices" />
-              )}
-            </div>
-          </>
-        )}
+          <div className="product-page__slider">
+            {newestProducts.length > 0 && (
+              <ProductsSlider phones={newestProducts} title="Hot prices" />
+            )}
+          </div>
+        </>
+      )}
     </>
   );
 };
