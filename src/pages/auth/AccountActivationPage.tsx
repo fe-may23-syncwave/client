98a1b04d4a901ed8ed114d1870bf669840f95b3a
fawnique <stolpnik.s.i@gmail.com>
2023-09-07 13:12:18 +0300

    Added ui for account-activation-page

--- src/pages/auth/AccountActivationPage.tsx
+++ src/pages/auth/AccountActivationPage.tsx
@@ -1,7 +1,11 @@
 import { Loader } from 'components/common/Loader';
-import { AuthContext } from 'context';
+import { MainContext, AuthContext } from 'context';
 import { useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import darkSuccessImg from '../../assets/images/darkEmail.png';
+import successImg from '../../assets/images/email.png';
+import errorImg from '../../assets/images/error-page.png';
+import { StylishComponent } from '../../components/StylishComponent';
 
 export const AccountActivationPage = () => {
   const [error, setError] = useState('');
@@ -9,6 +13,9 @@ export const AccountActivationPage = () => {
 
   const { activate } = useContext(AuthContext);
   const { activationToken } = useParams();
+  const { darkTheme } = useContext(MainContext);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!activationToken) {
@@ -30,21 +37,42 @@ export const AccountActivationPage = () => {
       });
   }, []);
 
+  let imageToUse;
+
+  if (error) {
+    imageToUse = errorImg;
+  } else {
+    imageToUse = darkTheme ? darkSuccessImg : successImg;
+  }
+
   if (!done) {
     return <Loader />;
   }
 
   return (
-    <>
-      <h1 className="title">Account activation</h1>
-
-      {error ? (
-        <p className="notification is-danger is-light">{error}</p>
-      ) : (
-        <p className="notification is-success is-light">
-          Your account is now active
-        </p>
+    <StylishComponent
+      imageSrc={imageToUse}
+      text={(
+        <>
+          <h1 className="title">Account activation</h1>
+          {error ? (
+            <p className="notification is-danger is-light">{error}</p>
+          ) : (
+            <>
+              <p className="notification is-success is-light">
+                Your account is now active
+              </p>
+              {/* eslint-disable-next-line react/button-has-type */}
+              <button
+                className="button is-primary"
+                onClick={() => navigate('/login')} // this will redirect to login page
+              >
+                Go to Login
+              </button>
+            </>
+          )}
+        </>
       )}
-    </>
+    />
   );
 };
